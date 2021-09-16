5df7ca362931db4ad931df2ecdb7d89f2a72a547
BuildTools <unconfigured@null.spigotmc.org>
2021-09-16 17:09:04 +0200

    Added analytics.

--- src/main/java/me/itsjeras/mcl_overseer/Analytics.java
+++ src/main/java/me/itsjeras/mcl_overseer/Analytics.java
@@ -3,7 +3,18 @@ package me.itsjeras.mcl_overseer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
@@ -39,25 +50,109 @@ public class Analytics implements Listener {
             try {
                 file.createNewFile();
                 FileManager.writeToFile("/Analytics/" + Get.CurrentDate().replace("/", "_") + ".yml",
-                        "Player-Stats:\n" +
-                        "# Connect\n" +
-                        "  Player-Connects: 0\n" +
-                        "  Player-Disconnects: 0\n" +
-                        "  Player-Handshakes: 0\n" +
-                        "# Health:\n" +
-                        "  Health-Lost: 0\n" +
-                        "  Health-Regenerated: 0\n" +
-                        "# Blocks:\n" +
-                        "  Blocks-Broken: 0\n" +
-                        "  Blocks-Built: 0\n");
-//                FileManager.writeToFile("PlayerData/" + Get.CurrentDate().replace("/", "_") + ".yml", "# File created on: " + Get.CurrentDate() + "\n" + Get.CurrentDate().replace("/", "_") + ":\n   Username: " + player.getDisplayName() + "\n   Honor: 100" + "\n   IP: " + player.getAddress().getHostName());
+                        "# Connection:\n"+
+                                "Player-Connects: 0\n"+
+                                "Player-Disconnects: 0\n"+
+                                "# Health:\n"+
+                                "Health-Lost: 0\n"+
+                                "Health-Regenerated: 0\n"+
+                                "# Blocks:\n"+
+                                "Blocks-Broken: 0 \n"+
+                                "Blocks-Built: 0\n"+
+                                "# Chat:\n"+
+                                "Messages-Sent: 0\n"+
+                                "Commands-Executed: 0\n"+
+                                "# Spawn:\n"+
+                                "Player-Deaths: 0\n"+
+                                "Entities-Spawned: 0\n");
             } catch (IOException exception) {
                 String fileName = Get.CurrentDate().replace("/", "_");
                 String message = "[> CheckForDataFile Exception (ANALYTICS)<] <DATE: " + Get.CurrentDate() + " TIME: " + Get.CurrentTime() + " Exception message: " + exception.getMessage();
                 FileManager.writeToFile("ExceptionLog/" + fileName + ".txt", "\n\n\n" + message);
             }
-        } else {
-//            SetIP(player);
         }
+    }
+
+//    public static void Save() {
+//        try {
+//            config.save(path);
+//        } catch (IOException exception) {
+//            LoggerInstance.severe(exception.getMessage());
+//        }
+//    }
+
+    // ANALYTICS:
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent event) throws IOException {
+        if(event.getEntity() instanceof Player ) {
+            double value = config.getInt("Health-Lost");
+            config.set("Health-Lost", value + event.getDamage());
+            config.save(path);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) throws IOException {
+        int value = config.getInt("Player-Deaths");
+        config.set("Player-Deaths", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void onPlayerJoinEvent(PlayerJoinEvent event) throws IOException {
+        int value = config.getInt("Player-Connects");
+        config.set("Player-Connects", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void onPlayerDisconnectEvent(PlayerQuitEvent event) throws IOException {
+        int value = config.getInt("Player-Disconnects");
+        config.set("Player-Disconnects", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler @Deprecated
+    public void onPlayerChat(PlayerChatEvent event) throws IOException {
+        int value = config.getInt("Messages-Sent");
+        config.set("Messages-Sent", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void onCommandExecution(PlayerCommandPreprocessEvent event) throws IOException {
+        int value = config.getInt("Commands-Executed");
+        config.set("Commands-Executed", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) throws IOException {
+        int value = config.getInt("Entities-Spawned");
+        config.set("Entities-Spawned", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void EntityRegainHealthEvent(EntityRegainHealthEvent event) throws IOException {
+        if(event.getEntity() instanceof Player ) {
+            double value = config.getDouble("Health-Regenerated");
+            config.set("Health-Regenerated", value + event.getAmount());
+            config.save(path);
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) throws IOException {
+        int value = config.getInt("Blocks-Built");
+        config.set("Blocks-Built", value + 1);
+        config.save(path);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) throws IOException {
+        int value = config.getInt("Blocks-Broken");
+        config.set("Blocks-Broken", value + 1);
+        config.save(path);
     }
 }
